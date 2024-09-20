e440c1b59567ea72c266d5166095260543aeeef7
itzraiyan <mdzilhazzarefin@gmail.com>
2024-09-20 11:54:42 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -233,7 +233,7 @@ async def delete_files(messages, client, k):
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
     # await client.send_message(messages[0].chat.id, "Your Video / File Is Successfully Deleted ✅")
-    await k.edit_text("<b>The posts were auto-removed by our bot to avoid copyright issues. You can still access them via the main link, but they'll be removed again in 3 days. ⏳</b>")
+    await k.edit_text("<b>The posts were auto-removed by our bot to avoid copyright issues. You can still access them via the main link, but they'll be removed again in 3 days⏳</b>")
 
 
 
